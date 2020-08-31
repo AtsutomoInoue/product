f28ec7c411262b83d459485e2e03884fc61be6be
Atsutomo Inoue <atsu@a02.itscom.net>
2020-08-31 22:46:52 +0900

    タスク管理

---
+++ Task/app/Http/Controllers/TasksController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TasksController extends Controller
+{
+    public function index()
+    {
+      //index.blade.phpファイルをレスポンスする
+      return view('tasks.index');
+    }
+}
