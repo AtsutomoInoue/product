0c90103e57e2eb167de71180ab1ef458e8dcda9a
Atsutomo Inoue <atsu@a02.itscom.net>
2022-06-15 22:37:48 +0900

    composer updated

---
+++ laravel/resources/views/menu.blade.php
@@ -0,0 +1,20 @@
+
+<div class="col-md-3">
+    <div class="card">
+        <div class="card-header"><i class="fas fa-th-list"><i></i><h1>メニュー</h1></div>
+        <div class="card-body">
+          <div class="panel panel-default">
+                <div class="alert alert-success" role="alert">
+                    <ul class="nav nav-pills nav-stacked" style="display:block;">
+                      <li><i class="fas fa-user-alt"></i>
+                        <a href="{{ url('/cyclings')}}">サイクリングに行った所</a></li>
+                      <li><i class="fas fa-user-alt"></i>
+                        <a href="{{ url('/plamodels')}}">作ったプラモデルの一覧</a></li>
+                      <li><i class="fas fa-user-alt"></i>
+                        <a href="{{ url('/posts')}}">掲示板</a></li>
+                    </ul>
+                </div>
+              </div>
+        </div>
+    </div>
+</div>
